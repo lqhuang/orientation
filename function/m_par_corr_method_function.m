bb0a1d7f49b0615b50f9f06467fbacb2e0884b61
Lanqing Huang <lqhuang@outlook.com>
2015-11-26 18:55:39 +0800

    complete parallel version of orientation function

--- function/m_par_corr_method_function.m
+++ function/m_par_corr_method_function.m
@@ -1,7 +1,18 @@
-function [subscript, varargout]= m_corr_method_function(exp_data, particle, pcimg_cell, pcimg_interpolation, weight)
-% information
-% in:
-% out:
+function [subscript, varargout]= m_par_corr_method_function(exp_projection, particle, pcimg_cell, pcimg_interpolation, weight)
+% parallel version of Correlation-ML method in orientation
+% input:
+% exp_projection: experiment image need to orientate
+% particle: struct of particle information. 
+%           PAY ATTENTION: all simulated projections are saved as
+%           matrix format. And the index increasing in order of
+%           phi(k), psi(j), thata(i)
+% pcimg_cell: all C2 image in polar cart should calculate previously
+% pcimg_interpolation: the interpolation method in creating C2 image.
+% weight: weight method in C2 image.
+% output:
+% subscript: the subscript in simulated_projection.
+% corr(optional): corrlation-maximum likelihood probability.
+% prob_k(optional): maximum likelihood probability in Phi angle.
 
 if exist('pcimg_interpolation', 'var') == 0;
     pcimg_interpolation = 'none';
@@ -15,13 +26,12 @@ ny = particle.simulated_size(2);
 nz = particle.simulated_size(3);
 
 Corr = zeros(1,nx*ny);
-C2_exp = m_corr_function_fft(exp_data, pcimg_interpolation, weight);
-C2_exp(1, :) = [];  % 第一行有极大的误差
+C2_exp = m_corr_function_fft(exp_projection, pcimg_interpolation, weight);
+C2_exp(1, :) = [];  % extreme error happens in first line
 
-for index = 1:nx*ny
-    [j,i]=ind2sub([ny,nx],index);
-    C2_sim = pcimg_cell{i,j};
-    C2_sim(1, :) = []; % 第一行有极大的误差
+parfor index = 1:nx*ny
+    C2_sim = pcimg_cell{index};
+    C2_sim(1, :) = []; % extreme error happens in first line
     scale_factor = C2_exp(:) \ C2_sim(:);
     Corr(index)=sum( sum( (C2_exp - scale_factor * C2_sim ) .^2 ./ (-2 * C2_exp) ) );
 end
@@ -31,35 +41,35 @@ end
 % maximum = max(Corr);
 % Corr = (Corr-minimum)./(maximum-minimum);
 
-
-% centrosymmetry effect
+% centrosymmetry problem
 [~, index_sort] = sort(Corr);
-index = index_sort(end-1 : end);
+index_of_max = index_sort(end-1 : end);
 
-[sub_j, sub_i] = ind2sub([ny, nx], index);
+[sub_i, sub_j] = ind2sub([nx, ny], index_of_max);
 
 Prob_k= zeros(length(sub_i), nz);
 max_prob_k = zeros(1, length(sub_i));
 sub_k = zeros(1, length(sub_i));
 
-index = 
-
 for n = 1: length(sub_i)
-    for k = 1:nz
-        Prob_k(n, k) = sum( sum( (exp_data - projection_cell{sub_i(n), sub_j(n), k}) .^2 ./ (-2 * exp_data) ) ) ;
+    simulated_projection_k = cell(1, nz);
+    simulated_projection_k(:) = particle.simulated_projection_k(sub_i(n), sub_j(n),:);
+    parfor k = 1:nz
+        Prob_k(n, k) = sum( sum( (exp_projection - simulated_projection_k{k}) .^2 ./ (-2 * exp_projection) ) ) ;
     end
     max_prob_k(n) = max(Prob_k(n, :));
-%     sub_k(n) = find( Prob_k(n, :) == max_prob_k(n) ); 原代码
-    %%%%%%%   第三个角度里面也有手性？？...bug不太好复现
+%     sub_k(n) = find( Prob_k(n, :) == max_prob_k(n) ); % 原代码
+%%% 第三个角度出现多个相同最大值？？...bug不太好复现
     t = find( Prob_k(n, :) == max_prob_k(n) ); %%???
     if length(t) ~= 1
-        sub_k(n) = t(1);%
+        sub_k(n) = t(1); 
+        disp('There are more than one max Phi angle found')
     else
-        sub_k(n) = find( Prob_k(n, :) == max_prob_k(n) );
+        sub_k(n) = t;
     end
 end
 
-max_sub = find( max_prob_k == max(max_prob_k) );
+max_sub =  max_prob_k == max(max_prob_k); % tricky method instead of ''max_sub = find( max_prob_k == max(max_prob_k) );''
 
 % normalization ???
 % minimum = min(max_prob_k);
--- function/m_par_ml_function.m
+++ function/m_par_ml_function.m
@@ -1,12 +1,22 @@
 function [subscript, varargout] = m_par_ml_function(exp_projection, particle)
+% parallel version of Maximum Likelihood method in orientation
+% input:
+% exp_projection: experiment image need to orientate
+% particle: struct of particle information. 
+%           PAY ATTENTION: all simulated projections are saved as
+%           cell format.
+% output:
+% subscript: the subscript in simulated_projection.
+% prob(optional): maximum likelihood probability.
 
 nx = particle.simulated_size(1);
 ny = particle.simulated_size(2);
 nz = particle.simulated_size(3);
 prob = zeors(1, nx*ny*nz);
+simulated_projection = particle.simulated_projection;
 
-for index = 1: nx*ny*nz
-    sim_projection = particle.simulated_projection(:,:,index);
+parfor index = 1: nx*ny*nz
+    sim_projection = simulated_projection{index};
     scale_factor = exp_projection(:) \ sim_projection(:);
     prob(index) = sum( sum( ( exp_projection - scale_factor * sim_projection ) .^2 ./ (-2 .* exp_projection) ) );
 end
@@ -16,8 +26,9 @@ end
 % maximum = max(prob);
 % likelihood = (prob-minimum)./(maximum-minimum);
 
-index = find(prob == max(prob));
-[sub_k, sub_j, sub_i] = ind2sub([nz, ny, nx], index);
+index_of_max = find(prob == max(prob));
+% [sub_k, sub_j, sub_i] = ind2sub([nz, ny, nx], max_index); % old method
+[sub_i, sub_j, sub_k] = ind2sub([nx, ny, nz], index_of_max);
 
 % output information
 subscript = [sub_i; sub_j; sub_k]';
