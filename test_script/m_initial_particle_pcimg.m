3c4e1d529f1aac16cea102efd5b7a5056e8488a0
lqhuang <lqhuang@outlook.com>
2015-12-05 10:01:23 +0800

    improve parallel performance

--- test_script/m_initial_particle_pcimg.m
+++ test_script/m_initial_particle_pcimg.m
@@ -4,10 +4,9 @@ function pcimg_cell = m_initial_particle_pcimg(particle, pcimg_method, weight)
 
 % create simulated projections
 % set intervals of siMLated projections
-step = particle.step;
 theta = particle.theta;
 psi = particle.psi;
-phi = particle.phi;
+% phi = particle.phi;
 
 % save projections into a cell
 num_theta = length(theta);
@@ -15,13 +14,13 @@ num_psi = length(psi);
 % num_phi = length(phi);
 
 pcimg_cell = cell(num_theta, num_psi);
+sim_projection_cell = cell(num_theta, num_psi);
+sim_projection_cell(:,:) = particle.simulated_projection(:,:,1);
 disp('begin to caculate projection');
-parfor i = 1: num_theta
-    for j = 1:num_psi
-        pcimg_cell{i,j} = m_corr_function_fft(particle.simulated_projection{i,j,1}, pcimg_method, weight);
-        disp(['i=',num2str(i),',j=',num2str(j),',k=',num2str(1)])
-    end
+parfor index = 1 : num_theta * num_psi
+    pcimg_cell{index} = m_corr_function_fft(sim_projection_cell{index}, pcimg_method, weight);
+    [i,j] = ind2sub([num_theta, num_psi], index);
+    disp(['i=',num2str(i),',j=',num2str(j),',k=',num2str(1)])
 end
-
 
 end
