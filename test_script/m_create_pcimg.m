aff7cd678399cca369ebe221095db0bdf99a5b61
Lanqing Huang <lqhuang@outlook.com>
2016-02-19 23:29:19 +0800

    debug

--- test_script/m_create_pcimg.m
+++ test_script/m_create_pcimg.m
@@ -2,21 +2,11 @@
 % particle = ? ;
 
 tic
-pcimg_cell = m_initial_particle_pcimg(particle, 'none', 'none');
+pcimg_cell = m_initial_particle_pcimg(particle, 'linear', 'none');
 toc
-save([path,'/corr_none_none.mat'], 'pcimg_cell');
+save([path,'/corr_linear_none.mat'], 'pcimg_cell');
 disp('OK')
 
-% tic
-% pcimg_cell = m_initial_particle_pcimg(particle, 'bilinear', 'none');
-% toc
-% save([path,'/corr_bilinear_none.mat'], 'pcimg_cell');
-% disp('OK')
-
-pcimg_cell = m_initial_particle_pcimg(particle, 'none', 'linear');
-save([path,'/corr_none_linear.mat'], 'pcimg_cell');
+pcimg_cell = m_initial_particle_pcimg(particle, 'linear', 'linear');
+save([path,'/corr_linear_linear.mat'], 'pcimg_cell');
 disp('OK')
-
-% pcimg_cell = m_initial_particle_pcimg(particle, 'bilinear', 'linear');
-% save([path,'/corr_bilinear_linear.mat'], 'pcimg_cell');
-% disp('OK')
--- test_script/m_runscript.m
+++ test_script/m_runscript.m
@@ -1,25 +1,44 @@
-m_instrument_noise_test
 run('/home/lqhuang/Documents/MATLAB/pathdef.m');
 addpath(ans)
 disp('add path successful!')
 
 step = 10;
 file = '/home/lqhuang/Documents/MATLAB/orientation/particle/EMD-6044.map';
-path = ['/mnt/data/lqhuang/EMD_6044_',num2str(step),'_fourier_125_125'];
-% path = ['/mnt/data/lqhuang/EMD_6044_',num2str(step)];
+path = ['/mnt/data/lqhuang/EMD_6044_',num2str(step),'_real_125_125_unnormalized_projector(linear)'];
 % run_path = pwd;
-% mkdir(path)
+mkdir(path)
 
-particle = m_initial_particle(file, 30.4, step, 'fft');
+particle = m_initial_particle_2(file, 30.4, step, 'real', 'linear');
 disp('initial successful!')
 save([path,'/EMD_6044_',num2str(step),'.mat'], 'particle');
 disp('save finish!')
-% load([path,'/EMD_6044_',num2str(step),'_fourier.mat'], 'particle');
-
-result_path = '/mnt/data/lqhuang/result/2016-01-20/';
 
 m_create_pcimg;
-load([path,'/corr_none_none.mat'], 'pcimg_cell');
 disp('pcimg create successful!')
 
-m_instrument_noise_test
+step = 10;
+file = '/home/lqhuang/Documents/MATLAB/orientation/particle/EMD-6044.map';
+path = ['/mnt/data/lqhuang/EMD_6044_',num2str(step),'_real_125_125_unnormalized_projector(cubic)'];
+% run_path = pwd;
+mkdir(path)
+
+particle = m_initial_particle_2(file, 30.4, step, 'real', 'cubic');
+disp('initial successful!')
+save([path,'/EMD_6044_',num2str(step),'.mat'], 'particle');
+disp('save finish!')
+
+step = 3;
+file = '/home/lqhuang/Documents/MATLAB/orientation/particle/EMD-6044.map';
+path = ['/mnt/data/lqhuang/EMD_6044_',num2str(step),'_real_125_125_unnormalized_projector(linear)'];
+% run_path = pwd;
+mkdir(path)
+
+particle = m_initial_particle_2(file, 30.4, step, 'real', 'linear');
+disp('initial successful!')
+save([path,'/EMD_6044_',num2str(step),'.mat'], 'particle');
+disp('save finish!')
+
+m_create_pcimg;
+disp('pcimg create successful!')
+
+
