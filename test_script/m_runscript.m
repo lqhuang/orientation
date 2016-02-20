66e3f7d4d837ed28f3cc8e3291cd4929e3028821
Lanqing Huang <lqhuang@outlook.com>
2016-02-20 23:17:34 +0800

    change runscript

--- test_script/m_runscript.m
+++ test_script/m_runscript.m
@@ -4,11 +4,11 @@ disp('add path successful!')
 
 step = 10;
 file = '/home/lqhuang/Documents/MATLAB/orientation/particle/EMD-6044.map';
-path = ['/mnt/data/lqhuang/EMD_6044_',num2str(step),'_real_125_125_unnormalized_projector(linear)'];
+path = ['/mnt/data/lqhuang/EMD_6044_',num2str(step),'_real_125_125_normalized_projector(linear)'];
 % run_path = pwd;
 mkdir(path)
 
-particle = m_initial_particle_2(file, 30.4, step, 'real', 'linear');
+particle = m_initial_particle(file, 30.4, step, 'real', 'linear');
 disp('initial successful!')
 save([path,'/EMD_6044_',num2str(step),'.mat'], 'particle');
 disp('save finish!')
@@ -18,27 +18,25 @@ disp('pcimg create successful!')
 
 step = 10;
 file = '/home/lqhuang/Documents/MATLAB/orientation/particle/EMD-6044.map';
-path = ['/mnt/data/lqhuang/EMD_6044_',num2str(step),'_real_125_125_unnormalized_projector(cubic)'];
+path = ['/mnt/data/lqhuang/EMD_6044_',num2str(step),'_real_125_125_normalized_projector(cubic)'];
 % run_path = pwd;
 mkdir(path)
 
-particle = m_initial_particle_2(file, 30.4, step, 'real', 'cubic');
+particle = m_initial_particle(file, 30.4, step, 'real', 'cubic');
 disp('initial successful!')
 save([path,'/EMD_6044_',num2str(step),'.mat'], 'particle');
 disp('save finish!')
 
 step = 3;
 file = '/home/lqhuang/Documents/MATLAB/orientation/particle/EMD-6044.map';
-path = ['/mnt/data/lqhuang/EMD_6044_',num2str(step),'_real_125_125_unnormalized_projector(linear)'];
+path = ['/mnt/data/lqhuang/EMD_6044_',num2str(step),'_real_125_125_normalized_projector(linear)'];
 % run_path = pwd;
 mkdir(path)
 
-particle = m_initial_particle_2(file, 30.4, step, 'real', 'linear');
+particle = m_initial_particle(file, 30.4, step, 'real', 'linear');
 disp('initial successful!')
 save([path,'/EMD_6044_',num2str(step),'.mat'], 'particle');
 disp('save finish!')
 
 m_create_pcimg;
 disp('pcimg create successful!')
-
-
