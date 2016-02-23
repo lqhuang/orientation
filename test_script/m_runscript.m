746b3d988852a17c65a6067f7545cd838e9955ae
lqhuang <lqhuang@outlook.com>
2016-02-23 10:10:05 +0800

    debug

--- test_script/m_runscript.m
+++ test_script/m_runscript.m
@@ -3,23 +3,23 @@ addpath(ans)
 disp('add path successful!')
 
 %% initial a particle
-step = 10;
-file = '/home/lqhuang/Documents/MATLAB/orientation/particle/EMD-6044.map';
-path = ['/mnt/data/lqhuang/EMD_6044_',num2str(step),'_fourier_125_125_unnormalized_projector(linear)'];
-% run_path = pwd;
-mkdir(path)
-
-particle = m_initial_particle_2(file, 30.4, step, 'fourier', 'linear');
-disp('initial successful!')
-save([path,'/EMD_6044_',num2str(step),'.mat'], 'particle');
-disp('save finish!')
-
-m_create_pcimg;
-disp('pcimg create successful!')
+% step = 10;
+% file = '/home/lqhuang/Documents/MATLAB/orientation/particle/EMD-6044.map';
+% path = ['/mnt/data/lqhuang/EMD_6044_',num2str(step),'_fourier_125_125_unnormalized_projector(linear)'];
+% % run_path = pwd;
+% mkdir(path)
+% 
+% particle = m_initial_particle_2(file, 30.4, step, 'fourier', 'linear');
+% disp('initial successful!')
+% save([path,'/EMD_6044_',num2str(step),'.mat'], 'particle');
+% disp('save finish!')
+% 
+% m_create_pcimg;
+% disp('pcimg create successful!')
 
 %% do test 1
-% do test 2
 step = 10;
+space = 'real';
 path = ['/mnt/data/lqhuang/EMD_6044_',num2str(step),'_real_125_125_unnormalized_projector(linear)'];
 load([path,'/EMD_6044_',num2str(step),'.mat'], 'particle');
 load([path,'/corr_linear_none.mat'], 'pcimg_cell');
