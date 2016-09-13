f6f38855fbfa36c2d125e04c49c696c826038ab1
lqhuang <lqhuang@outlook.com>
2016-09-13 16:02:24 +0800

    Revert "Fix bug: wrong radius of even image"
    This reverts commit 747e94e4cdeec64c932b913a9ab43e74d41f7660.

--- function/m_corr_function_fft.m
+++ function/m_corr_function_fft.m
@@ -31,7 +31,7 @@ function C2 = m_corr_function_fft(img, pcimg_interpolation, weight)
             else
                 [xs, ys]=meshgrid(1:nx,1:ny);
             end
-            radius = min([nx-cy, ny-cx])-1;
+            radius = min([nx-cy, ny-cx]);
             pcimg = m_imgpolarcoord3(img, xs, ys, cx, cy, radius, radius, 360);
         case 'bilinear'
             pcimg = m_ImToPolar(img, 0, 1, 60, 360);
